6a64aef5b30e233f9a2aa89b1bbbb94d4ac77025
Mitch Snow <snow801mitch@gmail.com>
2019-05-19 15:27:03 -0600

    Imported Link from react router dom and implements Link into the Nav.js file. Also added some comments as to what Link does. Since the hyperlinks appear blue, I added a const navStyle to change color to white

--- src/Nav.js
+++ src/Nav.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-  return (
-    <nav className="Nav">
-      <ul className="Navigation">
-          <li>About</li>
-          <li>Gallery</li>
-          <li>Contact</li>
-      </ul>
-    </nav>
-  );
+    const navStyle = {
+        color: 'white'
+    /* this const navStyle is to change the link color from the regular
+    blue hyperlink color to "white". (there is probably an easier way...) */
+    };
+
+    return (
+        <nav className="Nav">
+            <ul className="Navigation">
+                <Link style={navStyle} to="/about"><li>About</li></Link>
+                <Link style={navStyle} to="/gallery"><li>Gallery</li></Link>
+                <Link style={navStyle} to="/contact"><li>Contact</li></Link>
+                {/* Link creates a hyper link for the pages and makes them 'clickable'. Like Route, Link needs to be imported and 
+            has a to="###" to show the path or what it will hyper link with. */}
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
